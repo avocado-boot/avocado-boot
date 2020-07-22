e696d1a210c7895036633656e3a458331591aa2a
1224999091@qq.com <1224999091@qq.com>
2020-07-22 09:37:28 +0800

    初始化

--- avocado-boot-starter-core/src/main/java/com/avocado/boot/starter/core/enums/SystemErrorType.java
+++ avocado-boot-starter-core/src/main/java/com/avocado/boot/starter/core/enums/SystemErrorType.java
@@ -2,6 +2,7 @@ package com.avocado.boot.starter.core.enums;
 
 public enum SystemErrorType implements ErrorType {
   CUSTOM_ERROR("自定义异常信息."),
+  UNAUTHORIZED_EXCEPTION("无权访问."),
   SYSTEM_ERROR("系统异常，请联系管理员或稍后重试.");
 
   /**
