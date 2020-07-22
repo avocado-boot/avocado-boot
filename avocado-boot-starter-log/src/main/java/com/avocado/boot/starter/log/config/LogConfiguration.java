d6de8d9441c583275ae450c7bd036e695ff96b22
1224999091@qq.com <1224999091@qq.com>
2020-07-22 15:04:54 +0800

    初始化

--- avocado-boot-starter-log/src/main/java/com/avocado/boot/starter/log/config/LogConfiguration.java
+++ avocado-boot-starter-log/src/main/java/com/avocado/boot/starter/log/config/LogConfiguration.java
@@ -1,20 +1,22 @@
 package com.avocado.boot.starter.log.config;
 
 import com.avocado.boot.starter.log.annotation.LogConfigurerSupport;
+import com.avocado.boot.starter.log.aop.LogAspect;
 import com.avocado.boot.starter.log.factory.LogConfigurationAdapter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author ：qiaoliang
  * @date ：2020-07-22
  */
 @ComponentScan(basePackages = {
-        "com.avocado.boot.starter.log.service",
-        "com.avocado.boot.starter.log.aop"
+        "com.avocado.boot.starter.log.service"
 })
+@Import(LogAspect.class)
 @Configuration
 public class LogConfiguration {
 
